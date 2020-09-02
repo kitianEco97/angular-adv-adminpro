02356aecb12bce6d5010b0e2337f5cc5cf2ccc1e
Christian Morales <christianmosa123321@gmail.com>
2020-09-01 22:25:15 -0400

    Login y Auth

--- src/app/pages/pages.routing.ts
+++ src/app/pages/pages.routing.ts
@@ -1,6 +1,8 @@
 import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 
+import { AuthGuard } from '../guards/auth.guard';
+
 import { PagesComponent }  from './pages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
@@ -9,10 +11,12 @@ import { AccountSettingsComponent } from './account-settings/account-settings.co
 import { PromisesComponent } from './promises/promises.component';
 import { RxjsComponent } from './rxjs/rxjs.component';
 
+
 const routes: Routes = [
     { 
         path: 'dashboard', 
         component: PagesComponent,
+        canActivate: [AuthGuard],
         // Rutas hijas
         // Estas rutas se condicionan a los estilos mas que nada
         children: [
